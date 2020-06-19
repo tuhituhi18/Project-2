2b9322145feec5b9394c50dcd27aae25a633c64b
Alyssa Schmalenberger <aschmalenberger@gmail.com>
2020-06-18 22:46:19 -0500

    have reset center to be houston, and have written in pseudo code on where to pull in info from menu api

--- public/js/maps.js
+++ public/js/maps.js
@@ -1,34 +1,43 @@
+//var menu = require("menu.js")
 
 var map;
+
+
 function initMap() {
-    var locations = [
-        ['Bondi Beach', -33.890542, 151.274856, 4],
-        ['Coogee Beach', -33.923036, 151.259052, 5],
-        ['Cronulla Beach', -34.028249, 151.157507, 3],
-        ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
-        ['Maroubra Beach', -33.950198, 151.259302, 1]
+    
+    const locations = [
+        //this will be an empty array filled with info from menu search
+        ["Hughies", 29.7982, -95.3833],
+        ["ban oui", 29.7341, -95.3997],
     ];
 
     map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: -33.92, lng: 151.25 },
-        zoom: 8,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+        center: { lat: 29.76, lng: -95.36 },
+        zoom: 10,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+    
     });
     
-    var infowindow = new google.maps.InfoWindow();
+    
 
     var marker, i;
     for (i = 0; i < locations.length; i++) {  
         marker = new google.maps.Marker({
+            //get results to pull from locations once we figure out how that info is coming in
             position: new google.maps.LatLng(locations[i][1], locations[i][2]),
             map: map,
             
         });
     }
 
+    var infowindow = new google.maps.InfoWindow({
+        //need to figure out why name isnt working?
+        content: locations[i][0],
+    });
+
     google.maps.event.addListener(marker, 'click', (function(marker, i) {
         return function() {
-            infowindow.setContent(locations[i][0]);
+            // infowindow.setContent(locations[i][0]);
             infowindow.open(map, marker);
         }
     })(marker, i));
