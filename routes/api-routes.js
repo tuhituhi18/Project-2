f7c792dd59243d040aededcf22968854827e1267
Alyssa Schmalenberger <aschmalenberger@gmail.com>
2020-06-21 11:27:02 -0500

    work on data and routes, still need to import michale's changes to make sure these are working properly, mostly safe revert point but files need to be cleaned up

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -36,6 +36,23 @@ module.exports = function(app) {
     res.redirect("/");
   });
 
+  app.post("/api/user_data", (req, res) => {
+    console.log(req.body)
+    db.UserFavorite.create({
+      restaurantName: req.body.restaurantName,
+      foodName: req.body.foodName,
+      geoLat: req.body.geoLat,
+      geoLon: req.body.geoLon,
+    })
+    .then(()=>{
+      res.status(201);
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(401).json(err);
+    });
+  
+  })
   // Route for getting some data about our user to be used client side
   app.get("/api/user_data", (req, res) => {
     if (!req.user) {
@@ -45,8 +62,8 @@ module.exports = function(app) {
       // Otherwise send back the user's email and id
       // Sending back a password, even a hashed password, isn't a good idea
       res.json({
-        email: req.user.email,
-        id: req.user.id
+        // email: req.user.email,
+        // id: req.user.id
       });
     }
   });
